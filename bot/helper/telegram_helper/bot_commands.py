093db01b627b3c05ab7fb949f2562688d6473c03
Syed Murtaza Ali Naqvi <86062433+Murtazaaly01@users.noreply.github.com>
2021-12-03 21:25:10 +0500

    Initial commit

---
+++ bot/helper/telegram_helper/bot_commands.py
@@ -0,0 +1,85 @@
+class _BotCommands:
+
+    def __init__(self):
+
+        self.StartCommand = 'start'
+
+        self.MirrorCommand = 'mir2'
+
+        self.UnzipMirrorCommand = 'unzipmir2'
+
+        self.ZipMirrorCommand = 'zipmir2'
+
+        self.CancelMirror = 'cancel2'
+
+        self.CancelAllCommand = 'cancel2all'
+
+        self.ListCommand = 'list2'
+
+        self.SearchCommand = 'search2'
+
+        self.StatusCommand = 'status2'
+
+        self.AuthorizedUsersCommand = 'users2'
+
+        self.AuthorizeCommand = 'auth2'
+
+        self.UnAuthorizeCommand = 'unauth2'
+
+        self.AddSudoCommand = 'addsudo2'
+
+        self.RmSudoCommand = 'rmsudo2'
+
+        self.PingCommand = 'ping2'
+
+        self.RestartCommand = 'restart2'
+
+        self.StatsCommand = 'stats2'
+
+        self.HelpCommand = 'help2'
+
+        self.LogCommand = 'log2'
+
+        self.SpeedCommand = 'speedtest2'
+
+        self.CloneCommand = 'clone2'
+
+        self.CountCommand = 'count2'
+
+        self.WatchCommand = 'watch2'
+
+        self.ZipWatchCommand = 'zipwatch2'
+
+        self.QbMirrorCommand = 'qbmir2'
+
+        self.QbUnzipMirrorCommand = 'qbunzipmir2'
+
+        self.QbZipMirrorCommand = 'qbzipmir2'
+
+        self.DeleteCommand = 'remove2'
+
+        self.ShellCommand = 'shell2'
+
+        self.ExecHelpCommand = 'exechelp2'
+
+        self.LeechSetCommand = 'leechset2'
+
+        self.SetThumbCommand = 'setthumb2'
+
+        self.LeechCommand = 'leech2'
+
+        self.UnzipLeechCommand = 'leechunzip2'
+
+        self.ZipLeechCommand = 'zipleech2'
+
+        self.QbLeechCommand = 'qbleech2'
+
+        self.QbUnzipLeechCommand = 'qbunzipleech2'
+
+        self.QbZipLeechCommand = 'qbzipleech2'
+
+        self.LeechWatchCommand = 'leechwatch2'
+
+        self.LeechZipWatchCommand = 'leechzipwatch2'
+
+BotCommands = _BotCommands()
